f7f12c7612d2f226c08979c36c6b80b4b9337263
vivek <vivek.koyani@gmail.com>
2022-04-28 00:32:29 +0530

    add code counter

---
+++ countdown/src/CountdownTimer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useCountdown } from './useCountdown';
+import ExpiredNotice from './ExpiredNotice';
+import ShowCounter from './ShowCounter';
+
+const CountDownTime = ({ targetDate }) => {
+    const [days, hours, minutes, seconds] = useCountdown(targetDate);
+
+    if (days + hours + minutes + seconds <= 0) {
+        return <ExpiredNotice />;
+    } else {
+        return (
+            <ShowCounter
+                days={days}
+                hours={hours}
+                minutes={minutes}
+                seconds={seconds}
+            />
+        )
+    }
+}
+
+export default CountDownTime;
